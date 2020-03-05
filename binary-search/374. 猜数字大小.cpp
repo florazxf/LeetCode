53c84346fa44d708d88ff7916e24af0fdd414fca
flora <30709391+florazxf@users.noreply.github.com>
2020-03-06 00:52:38 +0800

    Create 374. 猜数字大小.cpp

---
+++ binary-search/374. 猜数字大小.cpp
@@ -0,0 +1,51 @@
+/*
+
+我们正在玩一个猜数字游戏。 游戏规则如下：
+我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+每次你猜错了，我会告诉你这个数字是大了还是小了。
+你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+
+-1 : 我的数字比较小
+ 1 : 我的数字比较大
+ 0 : 恭喜！你猜对了！
+示例 :
+
+输入: n = 10, pick = 6
+输出: 6
+*/
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+
+        int left=1;
+        int right = n;
+
+        int x;
+
+        while(left<=right){
+
+            x=left+(right-left)/2;
+
+            if(guess(x)==0){ //猜对了 x==pick
+                return x;
+            }
+            else if(guess(x)==-1){//x>pick [1,x)
+                right = x-1;
+                
+            }
+            else if(guess(x)==1){//x<pick (x,right]
+                left = x+1;
+            }
+
+        }
+
+        return x;
+        
+    }
+};
