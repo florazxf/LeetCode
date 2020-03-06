7aff46de31eb315107ec562349e5f9be053c400d
flora <30709391+florazxf@users.noreply.github.com>
2020-03-06 11:03:57 +0800

    Create 面试题57 - II. 和为s的连续正数序列.cpp

---
+++ 每日一题/面试题57 - II. 和为s的连续正数序列.cpp
@@ -0,0 +1,59 @@
+/*
+输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+
+序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+示例 1：
+输入：target = 9
+输出：[[2,3,4],[4,5]]
+示例 2：
+输入：target = 15
+输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+ 
+
+限制：
+1 <= target <= 10^5
+
+*/
+
+
+
+//方法一：暴力法（自己的方法
+class Solution {
+public:
+    vector<vector<int>> findContinuousSequence(int target) {
+
+
+        vector<int> eveans;
+
+        vector<vector<int>> ans;
+        int i = 1;
+        int sum=0;
+        int begin=1;
+        int end;
+        while(begin<target){
+            
+            sum+=i;
+            i++;
+            if(sum==target){
+                end=i;
+                for(int k=begin;k<end;k++){
+                    eveans.push_back(k);
+                }
+                ans.push_back(eveans);
+                eveans.clear();
+                
+            }
+            else if(sum>target){
+
+                begin++;
+                i=begin;
+                sum=0;
+            }
+            
+        }
+
+        return ans;
+
+    }
+};
