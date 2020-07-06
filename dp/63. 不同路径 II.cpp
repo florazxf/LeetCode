e740228a00a7a4d57ff68582688c5048ba507d8f
flora <30709391+florazxf@users.noreply.github.com>
2020-07-07 00:52:05 +0800

    Update 63. 不同路径 II.cpp

--- dp/63. 不同路径 II.cpp
+++ dp/63. 不同路径 II.cpp
@@ -132,3 +132,32 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+
+//7.7做每日一题
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        vector<vector<int>> dp(m,vector<int>(n,1));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0&&j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                dp[i][j] = (i>=1?dp[i-1][j]:0)+(j>=1?dp[i][j-1]:0);
+                cout<<i<<j<<dp[i][j]<<endl;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
