ae4fd1a5cf3a4c146f59d4802b1e1c00b5f3292a
flora <30709391+florazxf@users.noreply.github.com>
2020-07-13 10:35:03 +0800

    Update and rename 两个数组的交集 II.py to 两个数组的交集 II.cpp

--- 数组/两个数组的交集 II.cpp
+++ 数组/两个数组的交集 II.cpp
@@ -1,4 +1,4 @@
-'''
+/*
   两个数组的交集 II
 给定两个数组，编写一个函数来计算它们的交集。
 
@@ -19,7 +19,10 @@
 如果给定的数组已经排好序呢？你将如何优化你的算法？
 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
-'''
+
+*/
+
+//10个月以前用python做的
 class Solution(object):
     def intersect(self, nums1, nums2):
         """
@@ -41,3 +44,33 @@ class Solution(object):
                     nums3.append(num)
                     nums1.remove(num)
         return nums3
+      
+//7.13号刷每日一题的时候自己用c++做的方法
+/*
+1. 先给两个数组排序
+2. 分别给nums1和nums2一个指针，比较值的大小，值小的指针右移
+*/
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int l1 = 0,l2=0;
+        while(l1<nums1.size()&&l2<nums2.size()){
+            if(nums1[l1]==nums2[l2]){
+                ans.push_back(nums1[l1]);
+                l1++;
+                l2++;
+            }
+            else if(nums1[l1]<nums2[l2]){
+                l1++;
+            }
+            else{
+                l2++;
+            }
+        }
+        return ans;
+
+    }
+};
