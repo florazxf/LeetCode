04b2573f208ca674739a005dc51849f508b6c1c4
flora <30709391+florazxf@users.noreply.github.com>
2020-12-14 17:11:47 +0800

    Create 39. 组合总和.java

---
+++ dfs/回溯/39. 组合总和.java
@@ -0,0 +1,76 @@
+/*
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。 
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7,
+所求解集为：
+[
+  [7],
+  [2,2,3]
+]
+示例 2：
+
+输入：candidates = [2,3,5], target = 8,
+所求解集为：
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+ 
+
+提示：
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+candidate 中的每个元素都是独一无二的。
+1 <= target <= 500
+*/
+
+//方法一：
+
+/*
+https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+1. 首先注意i要从begin开始 这样去重 即本层前面用过的结点 接下来往下递归的时候就不选了
+2. 为了剪枝，把target-candidates[i]<0 排除， 就要先排序
+*/
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        Arrays.sort(candidates);
+
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        ArrayList<Integer> path = new ArrayList<Integer>();
+
+        backtrack(candidates,0,0,target,path,ans);
+        return ans;
+
+    }
+    public void backtrack(int[] candidates, int begin,int depth,int target, List<Integer> path,List<List<Integer>> ans){
+        //终止条件
+
+        if(target==0){
+            ans.add(new ArrayList<Integer>(path));
+            return ;
+        }
+
+        for(int i=begin;i<candidates.length;i++){//从begin 开始 去重
+
+            if(target-candidates[i]<0){
+                break;
+            }
+            path.add(candidates[i]);
+            backtrack(candidates,i,depth+1,target-candidates[i],path,ans); //注意是i 因为可以重复选
+            path.remove(path.size()-1);
+        }
+        
+        
+    }
+}
