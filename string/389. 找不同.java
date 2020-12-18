fffa86fa0ca79b58008d80426630ece1f5150f9b
flora <30709391+florazxf@users.noreply.github.com>
2020-12-18 16:14:17 +0800

    Update 389. 找不同.java

--- string/389. 找不同.java
+++ string/389. 找不同.java
@@ -32,8 +32,65 @@
 t.length == s.length + 1
 s 和 t 只包含小写字母
 */
+//方法四：求和
+//因为只差一个字母 所以t所有字母求和后减去s所有字母求和的值就是 要求的
 
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int as = 0;
+        int at = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            as += s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            at+= t.charAt(i);
+        }
+        return (char) (at-as);
+    }
+}
+
+//方法三：位运算
+//把s和t看成一个串 那就是找出现奇数次的字母 出现偶数次的字母异或后就是0  0异或出现奇数次的字母得到的还是那个数
+//类似于136题
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int ret = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ret ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            ret ^= t.charAt(i);
+        }
+        return (char) ret;
+    }
+}
+
+//方法二： 哈希表 
+//因为只有26个字母 所以用int[]cnt 存每个字母出现的次数
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int [] cnt  = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            cnt[c-'a']++;
+        }
+
+        for(int i = 0;i<t.length();i++){
+            char c1 = t.charAt(i);
+            cnt[c1-'a']--;
+            if(cnt[c1-'a']<0){
+                return c1;
+            }
+        }
+
+        return t.charAt(0);
+    }
+}
 //方法一：
+
+//自己写的，把两个排序 然后再比较
 class Solution {
     public char findTheDifference(String s, String t) {
         char[] c1= s.toCharArray();
